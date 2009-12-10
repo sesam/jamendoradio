432a65439d626af9906ad3c9f86199eda62b6e4d
fredrik.schiller <fredrik.schiller@cb547de0-dada-11de-8d17-8328ca00869f>
2009-12-10 23:28:00 +0000

    Whoa! The site integration just got a lot fatter. Now I can't find any unsupported playbuttons (except the one in the flash player), after I added support for Jamendo playlists and their own version of radio.

--- trunk/jamendoradio/JamendoRadio/JamendoRadio.js
+++ trunk/jamendoradio/JamendoRadio/JamendoRadio.js
@@ -1,34 +1,20 @@
 function manipulatePage() {
-	overrideAlbumLinks("album_simple_play");
-	overrideAlbumLinks("album_browser_playbutton");
-	overrideAlbumLinks("album_page_big_play");
-	overrideAlbumLinks("container_artistpage_play");
-	overrideTrackLinks("trackstable_play");
+    overrideLinks("jambutton_miniactionicon_inner");
+    overrideLinks("jambutton_bigactionicon");
+    overrideLinks("album_simple_play");
+    overrideLinks("container_artistpage_play");
+    overrideLinks("visual_radio");
+    overrideLinks("jambutton_playbutton_maxi");
+    overrideLinks("g_playbutton");
+    overrideLinks("jambutton_smallactionicon");
+
 	createArtistRadio("info_artistapage_avatar");
 }
 
-function overrideTrackLinks(cssClass) {
+function overrideLinks(cssClass) {
 	var playButtons = document.body.getElementsByClassName(cssClass);
 	for(i = 0; i < playButtons.length; i++) {
-		playButtons[i].onclick = function() { 
-			chrome.extension.sendRequest({target: "loadTrackPlayList", data: /id=(\d+)/.exec(this.innerHTML)[1]}, function(response) { }); 
-			return false; 
-		}
-		
-		var subLinks = playButtons[i].getElementsByTagName("a");
-		for(j = 0; j < subLinks.length; j++) {
-			if(subLinks[j].onclick) subLinks[j].onclick = "";
-		}
-	}
-}
-
-function overrideAlbumLinks(cssClass) {
-	var playButtons = document.body.getElementsByClassName(cssClass);
-	for(i = 0; i < playButtons.length; i++) {
-		playButtons[i].onclick = function() { 
-			chrome.extension.sendRequest({target: "loadAlbumPlayList", data: /album.(\d+)/.exec(this.innerHTML)[1]}, function(response) { }); 
-			return false; 
-		}
+	    playButtons[i].onclick = start;
 		
 		var subLinks = playButtons[i].getElementsByTagName("a");
 		for(j = 0; j < subLinks.length; j++) {
@@ -44,4 +30,34 @@ function createArtistRadio(cssClass) {
 		return false;
 	}
 }
+
+function start() {
+    var rxRadio = new RegExp("radio\\/(\\d+)'");
+    var rxTrack = new RegExp("\\?id=(\\d+)");
+    var rxAlbum = new RegExp("album\\/(\\d+)'");
+    var rxPlaylist = new RegExp("playlist\\/(\\d+)'");
+    var rxMisc = new RegExp("misc/([\\d+/]+)");
+    var rxMiscId = new RegExp("\\d+", "g");
+    var rxMiscIndex = new RegExp("/(\\d*)");
+
+    if (this.innerHTML.match(rxRadio))
+        chrome.extension.sendRequest({ target: "loadRadioPlayList", data: rxRadio.exec(this.innerHTML)[1] }, function (response) { });
+    else if (this.innerHTML.match(rxTrack))
+        chrome.extension.sendRequest({ target: "loadTrackPlayList", data: rxTrack.exec(this.innerHTML)[1] }, function (response) { });
+    else if (this.innerHTML.match(rxAlbum))
+        chrome.extension.sendRequest({ target: "loadAlbumPlayList", data: rxAlbum.exec(this.innerHTML)[1] }, function (response) { });
+    else if (this.innerHTML.match(rxPlaylist))
+        chrome.extension.sendRequest({ target: "loadPlayList", data: rxPlaylist.exec(this.innerHTML)[1] }, function (response) { });
+
+    else if (this.innerHTML.match(rxMisc)) {
+        var ids = rxMisc.exec(this.innerHTML)[1];
+        var index = parseInt(rxMiscIndex.exec(ids)[1]);
+
+        for (i = 0; i < index; i++) rxMiscId.exec(ids);
+        chrome.extension.sendRequest({ target: "loadTrackPlayList", data: rxMiscId.exec(ids)[0] }, function (response) { });
+    }
+
+    return false;
+}
+
 chrome.extension.sendRequest({target: "manipulatePage"}, function(response) { if(response.GoAhead) manipulatePage(); });
