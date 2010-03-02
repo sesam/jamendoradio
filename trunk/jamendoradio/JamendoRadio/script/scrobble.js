c76ad7e640a24df2a00004c5f189fcf4362dd5fa
fredrik.schiller <fredrik.schiller@cb547de0-dada-11de-8d17-8328ca00869f>
2010-03-02 18:02:39 +0000

    Scrobbling has a retry mechanism in case of connection failures.
    It still lacks a buffer for failed submissions, I'll look into that later. Then I think JR fully conforms to the API guidelines.
    
    Who's awesome?

--- trunk/jamendoradio/JamendoRadio/script/scrobble.js
+++ trunk/jamendoradio/JamendoRadio/script/scrobble.js
@@ -3,10 +3,11 @@
     var _clientId = clientId;
     var _clientVersion = clientVersion;
     var _session;
-    this.Handshake = function (user, passwordHash) {
+	
+    this.Handshake = function (user, passwordHash, fromRetry) {
         if (!allowed(true))return;
         if (!user || !passwordHash)return;
-        console.log('Sending handshake...');
+        if(!fromRetry) retry_reset();
         var ts = timeStamp();
         var auth_token = hex_md5(passwordHash + ts);
         var post_string = sformat("?hs=true&p=1.2.1&c={0}&v={1}&t={2}&u={3}&a={4}", _clientId, _clientVersion, ts, user, auth_token);
@@ -14,50 +15,30 @@
             var data = response.split('\n');
             if (data[0] == "OK") {
                 _session = { 'usr' : user, 'pwd' : passwordHash, 'sessionId' : data[1], 'npUrl' : data[2], 'subUrl' : data[3]};
-                console.log("Handshake completed, " + user + " is now logged in to endpoint " + _endpoint + ".");
-            }
-            else {
-                console.log("Request failed");
-                _session = false;
-            }
-            console.log("Response: " + response);
+            } else  fail(); 
         }
-        makeRequest("GET", _endpoint + post_string, setup);
+        makeRequest("GET", _endpoint + post_string, setup, fail	);
     }
     this.NowPlaying = function (cd) {
         if (!allowed() || !cd)return;
         var post_string = sformat("?s={0}&a={1}&b={2}&t={3}&l={4}&n={5}&m={6}", _session.sessionId, cd.a, cd.b, cd.t, cd.l, cd.n, cd.m);
-        console.log('Submitting NowPlaying for track "' + cd.t + '".');
         makeRequest("POST", _session.npUrl + post_string, function (r) {
-            console.log("Response: " + r);
-        }
-        , function () {
-            _session = false;
-        }
-        );
+			if(r.split('\n')[0] != "OK") fail();
+         }, fail);
     }
-    this.Submit = function (cd) {
+    this.Submit = function (cd, playTime) {
         if (!allowed())return;
-        // Check if scrobbling is allowed
-        if (cd.l < 30)console.log('Track "' + cd.t + '" is too short for scrobbling.');
-        var cts = timeStamp();
-        if ((cts - cd.i >= 240) || (cts - cd.i > cd.l / 2)) {
+        // Check if scrobbling is permitted in accordance with API guidelines
+        if (cd.l < 30) return;
+        if ((playTime >= 240) || (playTime > cd.l / 2)) {
             //All-righty then
             var post_string = sformat("?s={0}&o[0]=P&r[0]=&a[0]={1}&b[0]={2}&t[0]={3}&l[0]={4}&n[0]={5}&i[0]={6}&m[0]={7}", _session.sessionId, cd.a, cd.b, cd.t, cd.l, cd.n, cd.i, cd.m);
-            console.log('Submitting scrobble for track "' + cd.t + '".');
             makeRequest("POST", _session.subUrl + post_string, function (r) {
-                console.log("Response: " + r);
-            }
-            , function () {
-                _session = false;
-            }
-            );
+				if(r.split('\n')[0] != "OK") fail();
+           }, fail);
             return true;
         }
-        else {
-            //Guess not
-            console.log('It is way too soon to scrobble track "' + cd.t + '".');
-        }
+        //Guess not
         return false;
     }
     function allowed(skipSessionCheck) {
@@ -66,6 +47,20 @@
     this.IsReady = function () {
         return allowed();
     }
+	
+	var _retryCounter = 0;
+	var _retryTimer = null;
+	function retry(tries, user, passwordHash) {
+		if(tries < 3) _retryTimer = setTimeout(function() {  this.Handshake(user, passwordHash, true) }, 1500);
+	}
+	function retry_reset(){
+		if(_retryTimer) clearTimeout(_retryTimer);
+		_retryCounter = 0;
+	}
+	function fail() {
+		retry(_retryCounter++);
+		_session = false;
+	}
 }
 function getScrobbleObject(data, variant) {
     var scrobbleObject;
